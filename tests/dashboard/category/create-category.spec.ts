126ade188ffba77aee062e36f90ce5e405bbba70
Phong Do <dominhphong306@gmail.com>
2025-08-02 22:22:50 +0700

    chore: add some logs on test 1

--- tests/dashboard/category/create-category.spec.ts
+++ tests/dashboard/category/create-category.spec.ts
@@ -1,9 +1,11 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 
 test.beforeEach(async () => {
     console.log('Test changes');
 })
-test('test 1', async ({ page }) => {
 
+test('test 1', async ({ page }) => {
+    console.log("Running test 1");
+    expect(1).toEqual(1);
 })
